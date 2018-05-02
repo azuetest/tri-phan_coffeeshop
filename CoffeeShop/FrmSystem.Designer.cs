2c2b54a24bf5051802f51c87b9dbf21bc42d94be
Tri Phan <azuetest@gmail.com>
2018-05-02 09:32:58 +0700

    [NEW] Coffee Shop project  commit code
    Add new coffee shop project

---
+++ CoffeeShop/FrmSystem.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace CoffeeShop
+{
+    partial class FrmSystem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuSystem = new System.Windows.Forms.MenuStrip();
+            this.system_System_MenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loginToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.logoutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.systemLoggingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.accoutManagerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusSystem = new System.Windows.Forms.StatusStrip();
+            this.menuSystem.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuSystem
+            // 
+            this.menuSystem.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.system_System_MenuItem});
+            this.menuSystem.Location = new System.Drawing.Point(0, 0);
+            this.menuSystem.Name = "menuSystem";
+            this.menuSystem.Size = new System.Drawing.Size(912, 24);
+            this.menuSystem.TabIndex = 0;
+            this.menuSystem.Text = "menuStrip1";
+            // 
+            // system_System_MenuItem
+            // 
+            this.system_System_MenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loginToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.logoutToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.systemLoggingToolStripMenuItem,
+            this.toolStripSeparator3,
+            this.accoutManagerToolStripMenuItem,
+            this.toolStripSeparator4,
+            this.exitToolStripMenuItem});
+            this.system_System_MenuItem.Name = "system_System_MenuItem";
+            this.system_System_MenuItem.Size = new System.Drawing.Size(57, 20);
+            this.system_System_MenuItem.Text = "System";
+            // 
+            // loginToolStripMenuItem
+            // 
+            this.loginToolStripMenuItem.Name = "loginToolStripMenuItem";
+            this.loginToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.loginToolStripMenuItem.Text = "Login";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(159, 6);
+            // 
+            // logoutToolStripMenuItem
+            // 
+            this.logoutToolStripMenuItem.Name = "logoutToolStripMenuItem";
+            this.logoutToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.logoutToolStripMenuItem.Text = "Logout";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(159, 6);
+            // 
+            // systemLoggingToolStripMenuItem
+            // 
+            this.systemLoggingToolStripMenuItem.Name = "systemLoggingToolStripMenuItem";
+            this.systemLoggingToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.systemLoggingToolStripMenuItem.Text = "System Logging";
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(159, 6);
+            // 
+            // accoutManagerToolStripMenuItem
+            // 
+            this.accoutManagerToolStripMenuItem.Name = "accoutManagerToolStripMenuItem";
+            this.accoutManagerToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.accoutManagerToolStripMenuItem.Text = "Accout Manager";
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(159, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // statusSystem
+            // 
+            this.statusSystem.Location = new System.Drawing.Point(0, 440);
+            this.statusSystem.Name = "statusSystem";
+            this.statusSystem.Size = new System.Drawing.Size(912, 22);
+            this.statusSystem.TabIndex = 1;
+            this.statusSystem.Text = "statusStrip1";
+            // 
+            // FrmSystem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(912, 462);
+            this.Controls.Add(this.statusSystem);
+            this.Controls.Add(this.menuSystem);
+            this.MainMenuStrip = this.menuSystem;
+            this.Name = "FrmSystem";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmSystem";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuSystem.ResumeLayout(false);
+            this.menuSystem.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuSystem;
+        private System.Windows.Forms.StatusStrip statusSystem;
+        private System.Windows.Forms.ToolStripMenuItem system_System_MenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loginToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem logoutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem systemLoggingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripMenuItem accoutManagerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
